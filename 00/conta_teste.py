faf2c34d5ab93c60d57c54706e80bb94c5054fc3
Vitor <vvpt1@aluno.ifal.edu.br>
2025-10-27 22:44:08 -0300

    Questão 14

--- 00/conta_teste.py
+++ 00/conta_teste.py
@@ -1,6 +1,17 @@
 # Vitor Vinícius Porangaba Torres - 512
 
 from conta import Conta
-conta = Conta('123-45', 'Vini', 120.0, 1000.0)
-print(f'Número: {conta.numero}')
-print(f'Titular: {conta.titular}')
+conta1 = Conta('123-45', 'Vini', 120.0, 1000.0)
+conta2 = Conta('678-90', 'Vitor', 300.0, 2000.0)
+
+conta1.deposita(100)
+conta1.extrato()
+conta1.saca(200)
+conta1.extrato()
+conta2.deposita(50)
+conta2.extrato()
+conta2.saca(300)
+conta2.extrato()
+conta1.transfere_para(conta2, 20)
+conta1.extrato()
+conta2.extrato()
