626fb4976e39cdd09c7a69e78f3443c7852f8dce
Vitor <vvpt1@aluno.ifal.edu.br>
2025-11-13 08:29:53 -0300

    Questão 4 / exercício 9.5

--- 00/conta_teste.py
+++ 00/conta_teste.py
@@ -2,16 +2,19 @@
 
 from conta import Conta, Cliente, Historico
 
-cliente1 = Cliente("Vitor", "Torres", "111.222.333-44")
-conta1 = Conta('123-45', cliente1, 120.0, 1000.0)
-
+'''
 cliente2 = Cliente("Vinícius", "Porangaba", "555.666.777-88")
 conta2 = Conta('678-90', cliente2, 300.0, 2000.0)
 
-#print(conta1.__numero)
-#print(conta1._Conta__numero)
-
+print(conta1.__numero)
+print(conta1._Conta__numero)
 conta1.saldo = 120
 print(conta1.saldo)
-
 conta1.saldo = -30
+'''
+cliente1 = Cliente("Vitor", "Torres", "111.222.333-44")
+conta1 = Conta('123-45', cliente1, 120.0, 1000.0)
+
+print(conta1._numero, '\n')
+conta1._numero= '50' 
+print(conta1._numero)
